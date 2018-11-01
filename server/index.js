38c3f7e34bd7ed67f15c2002894d4c08b9297274
liyushilezhi <335136854@qq.com>
2018-11-01 18:35:31 +0800

    update

--- server/index.js
+++ server/index.js
@@ -9,8 +9,10 @@ async function start () {
   const app = new Koa()
   console.log('--------process.env.HOST-------');
   console.log(process.env);
-  const host = process.env.NODE_ENV=='production'? 'http://47.106.163.14': '127.0.0.1'
-  const port = process.env.NODE_ENV=='production'? 3002 : 3000
+  // const host = process.env.NODE_ENV=='production'? 'http://47.106.163.14': '127.0.0.1'
+  // const port = process.env.NODE_ENV=='production'? 3002 : 3000
+  const host ='127.0.0.1'
+  const port =3000
   app.on('error', function(err,ctx){
     console.log('-------统一错误打印-------');
     console.log(err);
